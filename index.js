6f89b9cb75575493c81ab97c1fa7440d05f2c610
Tarbiee <dorcusstaby@gmail.com>
2023-09-13 12:35:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Follow along with the examples here
+
+
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+console.log(say("Howdy","partner"))
+
+
+function say(fname, sname) {
+    console.log("I want to send message to!");
+    return `${fname}, ${sname}!`;
+  }
+console.log(say("Howdy","partner"))
+
